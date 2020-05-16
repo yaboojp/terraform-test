8f4bad3ad388004fd0689ec199662a25b8855065
yaboojp <yaboo.ja@gmail.com>
2020-05-16 15:50:10 +0900

    1st commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0c3fd0f5d33134a76"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "example"
+  }
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
+}
